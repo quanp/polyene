147687f25953d7cd09a89c0acd9e997c5d93e8dc
TCCM user 1 <tccm1@dirac.quantchem.kuleuven.be>
2018-03-26 18:39:11 +0200

    First commit

---
+++ c8/molcas.sh
@@ -0,0 +1,24 @@
+ulimit -s unlimited
+
+source /home/tccm1/source/molcas-extra/build/vars
+export PATH=/home/tccm1/source/CheMPS2/build-node87/CheMPS2:$PATH
+export LD_LIBRARY_PATH=/home/tccm1/source/CheMPS2/build-node87/CheMPS2:$LD_LIBRARY_PATH
+export PATH=/home/tccm1/bin:$PATH
+
+export MOLCAS_PROJECT="polyene" # use input as project name
+export MOLCAS_MEM=5000 # set memory size
+export MOLCAS_NPROCS=1
+export OMP_NUM_THREADS=1
+
+export CurrDir=$HOME/polyene/c8
+export WorkDir=/temp0/$USER/$MOLCAS_PROJECT.work
+export Project=$MOLCAS_PROJECT
+
+
+#### start the calculation ####
+rm -r $WorkDir
+
+cd $CurrDir
+
+molcas c8.input > c8.log
+
